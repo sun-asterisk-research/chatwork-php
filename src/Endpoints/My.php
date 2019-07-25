e2890c49769031d2145f5e6a85801af6379c7930
vunt-0906 <nguyen.tuan.vu@sun-asterisk.com>
2019-07-25 09:55:23 +0700

    Add My Status Endpoint

---
+++ src/Endpoints/My.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace SunAsterisk\Chatwork\Endpoints;
+
+
+class My extends Endpoint
+{
+    public function status()
+    {
+        return $this->api->get('my/status');
+    }
+
+}
