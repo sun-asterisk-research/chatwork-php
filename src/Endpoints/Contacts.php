61dac448c1661ae1260be2591d7f27befd071147
datbnt-0905 <nguyen.tien.datb@sun-asterisk.com>
2019-07-25 10:54:30 +0700

    add Contacts endpoint

---
+++ src/Endpoints/Contacts.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace SunAsterisk\Chatwork\Endpoints;
+
+class Contacts extends Endpoint
+{
+    public function __invoke()
+    {
+        return $this->api->get('contacts');
+    }
+}
---
+++ tests/Fixtures/contacts.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+        "account_id" => 123456,
+        "room_id" => 15651,
+        "name" => "Kunka",
+        "chatwork_id" => "",
+        "organization_id" => 123311,
+        "organization_name" => "abcdef",
+        "department" => "",
+        "avatar_image_url" => "",
+    ],
+    [
+        "account_id" => 123454,
+        "room_id" => 15588,
+        "name" => "Luna",
+        "chatwork_id" => "",
+        "organization_id" => 67890,
+        "organization_name" => "abcdef",
+        "department" => "",
+        "avatar_image_url" => "",
+    ],
+];
